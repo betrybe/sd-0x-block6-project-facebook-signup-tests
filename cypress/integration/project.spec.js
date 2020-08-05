024d346ed66dc6ad03cb8dbd6661f8461bde6e13
Renato Filho <renatosousafilho@gmail.com>
2020-08-05 17:58:53 -0300

    refatorando requisitos 1 a 13

--- cypress/integration/project.spec.js
+++ cypress/integration/project.spec.js
@@ -8,8 +8,9 @@ const USER_IDENTIFIER_LABEL_TEXT_SELECTOR = 'Email ou telefone';
 const USER_PASSWORD_LABEL_TEXT_SELECTOR = 'Senha';
 const USER_PASSWORD_INPUT_SELECTOR = 'input#user-password';
 const USER_LOGIN_BUTTON_SELECTOR = '#button-login';
+const FACEBOOK_SLOGAN_SELECTOR = 'p#facebook-slogan';
 const FACEBOOK_SLOGAN = 'O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.';
-const FACEBOOK_NETWORKING_IMG_SELECTOR = '#facebook-networking';
+const FACEBOOK_NETWORKING_IMG_SELECTOR = 'img#facebook-networking';
 const OPEN_ACCOUNT_MESSAGE = 'Abra uma conta';
 const QUICK_AND_SIMPLE_MESSAGE = 'É rápido e fácil.';
 const ALL_INPUT_SELECTOR = 'input';
@@ -51,82 +52,150 @@ const checkIsRightOf = (elementLeftSelector, elementRightSelector) => {
   });
 };
 
+const checkIsBelowOf = (elementAboveSelector, elementBelowSelector) => {
+  cy.document().then(doc => {
+    const elementAbove = {
+      top: evaluateOffset(doc, elementAboveSelector, 'top'),
+      height: evaluateOffset(doc, elementAboveSelector, 'height'),
+    };
+
+    const elementBelow = {
+      top: evaluateOffset(doc, elementBelowSelector, 'top'),
+      height: evaluateOffset(doc, elementBelowSelector, 'height'),
+    };
+
+    console.log(elementBelow.top)
+    console.log(elementAbove.top + elementAbove.height)
+
+    expect(elementBelow.top >= elementAbove.top + elementAbove.height).to.be.true;
+  });
+};
+
 
 describe('Facebook Signup', () => {
   beforeEach(() => {
     cy.visit('./index.html');
   });
 
-  describe('Posicionamento de elementos utilizando CSS Flexbox', () => {
-    it('Deve possuir algum seletor coma propriedade `display: flex`', () => {
-      cy.readFile('./style.css')
-        .then((content) => {
-          expect(content).to.match(/display: ?flex/);
-        });
-      });
-
-  });
-
   describe('Uma barra azul na parte superior da página do **Facebook** com a classe top-bar', () => {
-    it('Existe um elemento com a classe top-bar', () => {
+    it('Esta barra deve possuir a classe top-bar', () => {
       cy.get(TOP_BAR_SELECTOR).should('exist');
     });
 
+    it('A classe top-bar deve determinar que o elemento é um flex container', () => {
+      cy.get(TOP_BAR_SELECTOR).should('have.css', 'display', 'flex');
+    });
+
     it('A classe top-bar deve possuir a propriedade `background-color: rgb(66, 103, 178)`', () => {
-      cy.get('.top-bar').should('have.css', 'background-color', 'rgb(66, 103, 178)');
+      cy.get(TOP_BAR_SELECTOR).should('have.css', 'background-color', 'rgb(66, 103, 178)');
     });
   });
 
-  describe('O logotipo do Facebook no canto superior esquerdo com a classe facebook-logo', () => {
-    it('Existe um elemento img com a classe facebook-logo', () => {
+  describe('A barra superior deve conter o logotipo do Facebook no canto esquerdo com a classe facebook-logo', () => {
+    it('O logotipo deve estar alinhado a esquerda dentro da barra azul', () => {
+      cy.get(FACEBOOK_LOGOTIPO_SELECTOR).should('be.leftAligned', '.top-bar');
+    });
+
+    it('Deve existir um elemento img com a classe facebook-logo', () => {
       cy.get(FACEBOOK_LOGOTIPO_SELECTOR).should('exist');
     });
     
-    it('O atributo src deve apontar para ./imgs/facebook-logo.png', () => {
+    it('O atributo src do logotipo deve apontar para ./imgs/facebook-logo.png', () => {
       cy.get(FACEBOOK_LOGOTIPO_SELECTOR).should('have.attr', 'src').should('equal','./imgs/facebook-logo.png')
-    });
-
-    it('O elemento img deve estar alinhado a esquerda dentro da barra azul', () => {
-      cy.get('.facebook-logo').should('be.leftAligned', '.top-bar');
     });
   });
 
-  describe('Um formulário de autenticação (login) no canto superior direito', () => {
+  describe('A barra superior deve conter um formulário de autenticação no canto direito', () => {
+    it('O formulário deve estar alinhado a direita dentro da barra azul', () => {
+      cy.get(FACEBOOK_LOGIN_FORM_SELECTOR).should('be.rightAligned', '.top-bar');
+    });
+
     it('Existe formulário possui uma classe chamada facebook-login', () => {
       cy.get(FACEBOOK_LOGIN_FORM_SELECTOR).should('exist');
     });
 
+    it('O formulário deve ser um flex container', () => {
+      cy.get(FACEBOOK_LOGIN_FORM_SELECTOR).should('have.css', 'display', 'flex');
+    });
+  });
 
-    it('O formulário deve estar alinhado a direita dentro da barra azul', () => {
-      cy.get('.facebook-login').should('be.rightAligned', '.top-bar');
+  describe('Crie uma classe no CSS chamada form-group', () => {
+    it('Essa classe deve possuir a propriedade `diplay: flex`', () => {
+      cy.get('.form-group').should('have.css', 'display', 'flex');
     });
 
-    it('Um título com o texto "Email ou telefone" acima do campo de entrada de texto para email ou telefone com o id user-email-phone-label', () => {
-      cy.get(USER_IDENTIFIER_LABEL_SELECTOR)
+    it('Alinhe o eixo principal dessa classe para ser o eixo vertical', () => {
+      cy.get('.form-group').should('have.css', 'flex-direction', 'column');
+    });
+  });
+
+  describe('Dentro do formulário criado anteriormente, adicione o primeiro subcontainer do formulário para o campo "E-mail ou telefone"', () => {
+    it('Deve haver um container utilizando a classe `form-group` criada no passo anterior', () => {
+      cy.get('form.facebook-login').children().first()
         .should('exist')
-        .should('have.text', USER_IDENTIFIER_LABEL_TEXT_SELECTOR)
-        .should('be.above', USER_IDENTIFIER_INPUT_SELECTOR, 1);
+        .should('have.class', 'form-group')
     });
   
-    it('Um campo de entrada de texto no canto superior direito para receber o email ou o telefone do usuário com o id user-email-phone', () => {
-      cy.get(USER_IDENTIFIER_INPUT_SELECTOR).should('exist');
-    });
+    it('Dentro do container `form-group` criado, deve haver um rótulo com o id user-email-phone-label e o texto "Email ou telefone"', () => {
+      cy.get(USER_IDENTIFIER_LABEL_SELECTOR).parent().should('have.class', 'form-group');
 
-    it('Um título com o texto "Senha" acima do campo de entrada de texto para senha com o id user-password', () => {
-      cy.get(USER_PASSWORD_LABEL_SELECTOR)
+      cy.get(USER_IDENTIFIER_LABEL_SELECTOR)
         .should('exist')
-        .should('have.text', USER_PASSWORD_LABEL_TEXT_SELECTOR)
-        .should('be.above', USER_PASSWORD_INPUT_SELECTOR, 1);
+        .should('have.text', USER_IDENTIFIER_LABEL_TEXT_SELECTOR);
     });
 
-    it('Um campo de entrada de texto para digitar a senha do usuário, à direita do campo de entrada de texto para email ou telefone', () => {
-      cy.get(USER_PASSWORD_INPUT_SELECTOR).should('have.attr', 'type', 'password');
+    it('Dentro do container `form-group` criado, abaixo do rótulo deve haver campo de entrada de texto para receber o email ou o telefone do usuário com o id user-email-phone', () => {
+      cy.get(USER_IDENTIFIER_INPUT_SELECTOR).should('exist');
+      
+      cy.get(USER_IDENTIFIER_INPUT_SELECTOR).parent().should('have.class', 'form-group');
 
-      checkIsRightOf(USER_IDENTIFIER_INPUT_SELECTOR, USER_PASSWORD_INPUT_SELECTOR);
+      checkIsBelowOf(USER_IDENTIFIER_LABEL_SELECTOR, USER_IDENTIFIER_INPUT_SELECTOR);
+    });
+  });
 
+  describe('Dentro do formulário criado anteriormente, adicione o segundo subcontainer do formulário para o campo "Senha"', () => {
+    it('Deve haver um novo container utilizando a classe `form-group` criada no passo 4', () => {
+      cy.get("form.facebook-login > .form-group").eq(1).should('exist');
+    });
+  
+    it('Dentro do novo container `form-group` criado, deve haver um rótulo com o id user-password-label e o texto "Senha"', () => {
+      cy.get("form.facebook-login .form-group label#user-password-label")
+        .should('exist')
+        .should('have.text', USER_PASSWORD_LABEL_TEXT_SELECTOR);
     });
 
-    it('Um botão com o id "button-login" e o texto "Entrar", à direita do campo de entrada de texto para senha', () => {
+    it('Dentro do novo container `form-group` criado, abaixo do rótulo deve haver campo de entrada para senha com o id user-password', () => {
+      cy.get("form.facebook-login .form-group input#user-password").should('exist');
+
+      checkIsBelowOf(USER_PASSWORD_LABEL_SELECTOR, USER_PASSWORD_INPUT_SELECTOR);
+
+    });
+  });
+
+  describe('Crie uma classe no CSS chamada form-control', () => {
+    it('Essa classe deve possuir a propriedade `align-self: flex-end`', () => {
+      cy.get('.form-control').should('have.css', 'align-self', 'flex-end');
+    });
+  });
+
+  describe('Dentro do formulário criado anteriormente, adicione o terceiro subcontainer para o botão de login', () => {
+    it('Deve haver um novo container utilizando a classe `form-control` criada no passo anterior`', () => {
+      cy.get("form.facebook-login > .form-control").eq(0).should('exist');
+    });
+
+    it('Dentro do novo container `form-control` criado, deve haver um botão com o id "button-login" e o texto "Entrar"', () => {
+      cy.get("form.facebook-login .form-control #button-login")
+        .should('exist')
+        .should('have.text', 'Entrar');
+
+        checkIsRightOf(USER_PASSWORD_INPUT_SELECTOR, USER_LOGIN_BUTTON_SELECTOR);
+    });
+
+    it('O botão deve estar alinhado a direita do campo de entrada para senha', () => {
+      checkIsRightOf(USER_PASSWORD_INPUT_SELECTOR, USER_LOGIN_BUTTON_SELECTOR);
+    });
+
+    it('Ao clicar no botão com o id #button-login deve exibir um alert com o valor do campo "Email ou telefone"', () => {
       const content = 'my-user';
       let alerted = false;
   
@@ -135,8 +204,7 @@ describe('Facebook Signup', () => {
         alerted = true;
       });
 
-      checkIsRightOf(USER_PASSWORD_INPUT_SELECTOR, USER_LOGIN_BUTTON_SELECTOR);
-
+  
       cy.get(USER_IDENTIFIER_INPUT_SELECTOR).type(content);
       cy.get(USER_LOGIN_BUTTON_SELECTOR)
         .should('exist')
@@ -147,54 +215,90 @@ describe('Facebook Signup', () => {
     });
   });
 
-  
+
+  describe("Crie agora o conteúdo abaixo da barra superior com os seguintes itens", () => {
+    it('Crie um elemento após a barra lateral com a classe main-content (deve ser um flex container) ', () => {
+      cy.get('.main-content').should('have.css', 'display', 'flex');
+
+      checkIsBelowOf('.top-bar', '.main-content');
+    });
+
+    it("Crie dois elementos container para organizar o conteúdo de cada lado", () => {
+      cy.get('.main-content').children().eq(0).should('exist');
+      cy.get('.main-content').children().eq(1).should('exist');
+      cy.get('.main-content').children().eq(2).should('not.exist');
+    });
+
+    it('Utilize na classe main-content a propriedade justify-content com o valor mais apropriado para alinhar os conteúdos', () => {
+      cy.get('.main-content').should('have.css', 'justify-content');
+    });
+
+    it('Dentro do primeiro container existe um parágrafo com id facebook-slogan e o texto "O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida."', () => {
+      cy.get('.main-content').children().eq(0).get('#facebook-slogan').contains(FACEBOOK_SLOGAN);
+    });
+
+    it('Dentro do primeiro container, abaixo do texto do passo anterior existe uma imagem com id facebook-networking.', () => {
+      cy.get('.main-content').children().eq(0).get(FACEBOOK_NETWORKING_IMG_SELECTOR)
+        .should('exist')
+        .should(($el) => {
+          const src = $el.attr('src');
+          expect(src).to.match(/networking/);
+        });
+
+      checkIsBelowOf(FACEBOOK_SLOGAN_SELECTOR, FACEBOOK_NETWORKING_IMG_SELECTOR);
+    });
+
+    it('Dentro do segundo container, existe um texto "Abra uma conta"', () => {
+      cy.get('.main-content').children().eq(1).children().eq(0).contains(OPEN_ACCOUNT_MESSAGE);
+    });
+
+    it('Dentro do segundo container, existe abaixo do texto anterior um outro texto "É rápido e fácil." posicionado abaixo do texto "Abra uma conta"', () => {
+      cy.get('.main-content').children().eq(1).children().eq(1).contains(QUICK_AND_SIMPLE_MESSAGE);
+    });
+
+    it('Ainda dentro do segundo container, abaixo do texto anterior crie um elemento form', () => {
+      cy.get('.main-content').children().eq(1).children().eq(2).get('form').should('exist')
+    });
+  });
+
+  describe("Crie um campo de entrada de texto para o nome do usuário dentro do formulário criado no requisito 9", () => {
+    it('O campo deve ter o atributo name com o valor "firstname"', () => {
+      cy.get('.main-content form input[name="firstname"]').should('exist');    
+    });
+
+    it('O campo deve ter um placeholder com o valor "Nome"', () => {
+      cy.get('.main-content form input[name="firstname"]').should('have.attr', 'placeholder', 'Nome');
+    });
+  });
+
+  describe("Crie um campo de entrada de texto para o sobrenome do usuário dentro do formulário criado no requisito 9", () => {
+    it('O campo deve ter o atributo name com o valor "lastname"', () => {
+      cy.get('.main-content form input[name="lastname"]').should('exist');    
+    });
+
+    it('O campo deve ter um placeholder com o valor "Sobrenome"', () => {
+      cy.get('.main-content form input[name="lastname"]').should('have.attr', 'placeholder', 'Sobrenome');
+    });
+
+    it('Esse campo deve estar alinhado a direita do campo de Nome', () => {
+      checkIsRightOf('.main-content form input[name="firstname"]', '.main-content form input[name="lastname"]')
+    });
+  });
 
   
 
-  
+  // describe("Crie um campo de entrada de texto para o sobrenome do usuário dentro do formulário criado no requisito 9", () => {
+  //   it('Um campo de entrada de texto para o sobrenome do usuário. Posicione esse campo à direita do campo nome', () => {
+  //     cy.get(ALL_INPUT_SELECTOR)
+  //       .then(($inputs) => {
+  //         const elements = $inputs.toArray();
+  //         expect(checkPlaceholder(elements, 'Sobrenome')).to.be.true;
+  //       });
+  //   });
 
-  
-
-  // it('Um texto "O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida."', () => {
-  //   cy.contains(FACEBOOK_SLOGAN);
-  // });
-
-  // it('Uma imagem com id facebook-networking, que ficará abaixo do item 8. Essa imagem deve conter o mapa do mundo e as conexões entre as pessoas', () => {
-  //   cy.get(FACEBOOK_NETWORKING_IMG_SELECTOR)
-  //     .should('exist')
-  //     .should(($el) => {
-  //       const src = $el.attr('src');
-  //       expect(src).to.match(/networking/);
-  //     });
-  //   // assert position
-  // });
-
-  // it('Um texto "Abra uma conta" posicionado abaixo da caixa de texto de email/telefone', () => {
-  //   cy.contains(OPEN_ACCOUNT_MESSAGE);
-  //   // assert position
-  // });
-
-  // it('Um texto "É rápido e fácil." posicionado abaixo do texto "Abra uma conta"', () => {
-  //   cy.contains(QUICK_AND_SIMPLE_MESSAGE);
-  //   // assert position
-  // });
-
-  // it('Um campo de entrada de texto para o nome do usuário. Posicione esse campo abaixo do texto "É rápido e fácil."', () => {
-  //   cy.get(ALL_INPUT_SELECTOR)
-  //     .then(($inputs) => {
-  //       const elements = $inputs.toArray();
-  //       expect(checkPlaceholder(elements, 'Nome')).to.be.true;
-  //     });
-  //   // assert position
-  // });
-
-  // it('Um campo de entrada de texto para o sobrenome do usuário. Posicione esse campo à direita do campo nome', () => {
-  //   cy.get(ALL_INPUT_SELECTOR)
-  //     .then(($inputs) => {
-  //       const elements = $inputs.toArray();
-  //       expect(checkPlaceholder(elements, 'Sobrenome')).to.be.true;
-  //     });
-  //   // assert position
+  //   it('Posicione esse campo à direita do campo nome', () => {
+  //     checkIsRightOf()
+  //   });
   // });
 
   // it('Um campo de entrada de texto para o celular ou email. Posicione esse campo abaixo do sobrenome do usuário', () => {
